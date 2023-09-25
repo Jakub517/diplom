d02211cf3c4a37bebaf047b09f13d317a0f82fae
Jakub517 <pavlu.jakub8@seznam.cz>
2023-09-25 20:14:00 +0200

    opraveno

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const certifikateDiv = document.getElementById("certifikate");
 
 generateButton.addEventListener("click", () => {
     const code = codeInput.value;
-    var sifrovano = "U2FsdGVkX19JpXNnYMAZ4BPG1bN7L4YSF+RlIKNujHVyUZgvxpoURva/Af61Odfg"
+    var sifrovano = "U2FsdGVkX18HOm/6iglkh92eb/ghzyGaFg0UL5fOyPqD4khggn+Lqkd50GrA6RR0"
     var desifrovano = CryptoJS.AES.decrypt(sifrovano, code);
     try {
         var vysledek = desifrovano.toString(CryptoJS.enc.Utf8);
@@ -17,7 +17,7 @@ generateButton.addEventListener("click", () => {
         <p>Našli jste bonusovou kešku a získáváte certifikát o absolvování.</p>
         <p>Kód je: <b>${code}</b></p>
         <p>Datum zobrazení je: <b>${new Date().toLocaleDateString()}</b></p>
-        <p>Vaše souřadnice jsou:</p>
+        <p>Váš diplom je:</p>
         <img src="nfkdavnlkbvbdsbcvjbflkvnf.png" style="width: 80%" />
         <a href=${vysledek} download=${vysledek}>Stáhnout obrázek</a>
         `;
